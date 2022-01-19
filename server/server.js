86733eb49f17e38299cdbe2e20af3e3b3b4e55c7
Wahaj <wahajhaider32@gmail.com>
2022-01-19 13:35:44 -0500

    added correct redirect to server inside _redirects

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.send("Express app has started!!");
